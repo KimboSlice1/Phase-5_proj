efaddd8a38420f917d5cfb0d36c5c019d4ce1840
Kimber Herron <kh582013@gmail.com>
2023-01-17 16:09:59 -0500

    update student component

--- client/src/UserHomePage.js
+++ client/src/UserHomePage.js
@@ -3,13 +3,24 @@ import React from "react";
 
 function userHome(){
 
+const [data, setData]=useState([]);
+const [user, setUser]=useState(null)
 
+    fetch('/userinsession')
+        .then(r=>r.json())
+        .then(arrayOfData => setData(arrayOfData))
+
+
+    const addData = (data)=> setData(current => [...current, data])
+    const updateData = (user)=> setUser(user)
 
 
     
     return (
         <div className="UserHomePage">
             <h1> shit work? fuck</h1>
+            <
+            {/* put a button here to make you go to create data */}
             </div>
     )
 }
